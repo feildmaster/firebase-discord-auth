a8c36e4000b2a3230911e76c462c0af0a6f03f9d
feildmaster <feildmaster@users.noreply.github.com>
2021-01-04 00:37:59 -0600

    fix: deploy function script

--- scripts/cache_file.js
+++ scripts/cache_file.js
@@ -2,10 +2,16 @@ const { fileSync } = require('tmp');
 const { writeSync } = require('fs');
 const { getInput, setOutput } = require('./util/io');
 
+function writeTemp(file, ext = '.json') {
+  const tmp = fileSync({ postfix: ext });
+
+  writeSync(tmp.fd, file);
+
+  return tmp.name;
+}
+
 module.exports = () => {
-  const tmp = fileSync({ postfix: getInput('EXT') || '.json' });
-
-  writeSync(tmp.fd, getInput('CONTENT'));
-
-  setOutput('file', tmp.name);
+  setOutput('file', writeTemp(getInput('CONTENT'), getInput('EXT')));
 };
+
+module.exports.writeTemp = writeTemp;
---
+++ scripts/deploy_functions.js
@@ -0,0 +1,21 @@
+const { getInput } = require('./util/io');
+const cache_file = require('./cache_file').writeTemp;
+const exec = require('@actions/exec');
+
+const serviceAccount = getInput('SERVICE_ACCOUNT');
+const only = getInput('ONLY');
+
+if (!serviceAccount) throw new Error('Missing service account');
+
+const fileName = cache_file(serviceAccount);
+
+await exec('npx firebase-tools', [
+  'deploy',
+  '--only',
+  `functions${only ? `:${only}` : ''}`,
+], {
+  env: {
+    FIREBASE_DEPLOY_AGENT: 'github-actions',
+    GOOGLE_APPLICATION_CREDENTIALS: fileName,
+  },
+}).catch(console.error);
